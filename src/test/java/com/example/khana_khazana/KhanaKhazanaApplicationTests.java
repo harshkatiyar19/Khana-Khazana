458724653e1f1da156df035fd54c0e5639b8e912
harshkatiyar19 <harshkatiyar03.w@gmail.com>
2025-08-08 10:39:41 +0530

    initialize

---
+++ src/test/java/com/example/khana_khazana/KhanaKhazanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.khana_khazana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhanaKhazanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
