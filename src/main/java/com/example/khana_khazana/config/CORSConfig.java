be3808da86b70d796470e188ac639b602cd7e6fb
harshkatiyar19 <harshkatiyar03.w@gmail.com>
2025-08-29 19:03:03 +0530

    implementing cors

---
+++ src/main/java/com/example/khana_khazana/config/CORSConfig.java
@@ -0,0 +1,24 @@
+package com.example.khana_khazana.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CORSConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*"); // allow all for dev
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
